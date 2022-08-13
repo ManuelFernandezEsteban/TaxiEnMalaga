f2f1acb4664cd3e7e87ed31f253dc3a811920e38
Manuel1977 <Manuel_fe1977@hotmail.com>
2022-08-13 22:04:17 +0100

    cards visitas animation

--- cards.js
+++ cards.js
@@ -3,7 +3,7 @@ const visitas = document.querySelectorAll('.box-visitas');
 visitas.forEach(visita => {
     
    visita.addEventListener('click',function(){
-    console.log(this)
+   
     this.classList.toggle('descubierto');
    })
     
